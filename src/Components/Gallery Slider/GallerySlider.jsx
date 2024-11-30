0a13f85a586f4d0e2b06e39208e95b9131f2e641
Abdul Samad <samadcom797@gmail.com>
2024-11-30 13:37:50 +0500

    Working on slider

--- src/Components/Gallery Slider/GallerySlider.jsx
+++ src/Components/Gallery Slider/GallerySlider.jsx
@@ -1,5 +1,10 @@
 import { React } from 'react';
-  
+import { Slider } from 'react';
+
 function GallerySlider() {
-  
+  return (
+
+  );
 }
+
+export { GallerySlider };
---
+++ src/Components/Gallery Slider/Slider.jsx
@@ -0,0 +1,15 @@
+import { React } from 'react';
+
+function Slider({ sliderImages, autoplayCustomizaions }) {
+  return
+  {(
+      sliderImages.map((sliderImg) => {
+        return <div className="w-full">
+
+        </div>;
+      })
+    )};
+  
+}
+
+export { Slider };
